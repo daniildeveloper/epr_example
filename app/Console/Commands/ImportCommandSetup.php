9b468e13fa54058da0eace034e571469780db8d8
daniildeveloper <daniildeveloper@ya.ru>
2018-07-15 15:23:38 +0600

    In process commands setup

--- app/Console/Commands/ImportCommandSetup.php
+++ app/Console/Commands/ImportCommandSetup.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Action;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
+use League\Csv\Reader;
+use League\Csv\Statement;
 
 class ImportCommandSetup extends Command
 {
@@ -30,7 +30,7 @@ class ImportCommandSetup extends Command
     protected $columns = [];
 
     protected $files = [
-        './storage/app/public/2.csv'
+        './storage/app/public/2.csv',
     ];
     /**
      * Create a new command instance.
@@ -51,7 +51,15 @@ class ImportCommandSetup extends Command
     {
         // 1. Распознование колонок
         $this->line('Show columns...');
-        $this->line('Reading file ' . $this->file);
+
+        foreach ($this->files as $file) {
+            $columns = $this->columns_setup($file);
+
+            $this->line('Columns...');
+            foreach ($columns as $key => $value) {
+                $this->line("[$key] => $value");
+            }
+        }
 
         // 2. Вызов сущности модели
         // 3. Генерация поставок
@@ -59,5 +67,41 @@ class ImportCommandSetup extends Command
         // 6. Ввод товаров
     }
 
-    private function columns_setup() {}
+    private function columns_setup($file_path)
+    {
+        $csv     = Reader::createFromPath($file_path, 'r');
+        $columns = [];
+
+        $expected_columns_names = [
+            'Клиент',
+            'Кол-во',
+            'Марка',
+            'Грунтовка л',
+            'Пропитка л',
+            'Эластификатор',
+            'Скотч',
+            'Толщина',
+            'Сумма за г.п.э.с.',
+            'Сумма ',
+            'откат',
+            'бонус',
+            'Сумма очищенная',
+
+        ];
+
+        $stmt = (new Statement())->offset(0)->limit(1);
+
+        $records = $stmt->process($csv);
+
+        foreach ($records as $record) {
+            foreach ($record as $k => $value) {
+                $this->line("[$k] => $value");
+                if (in_array($value, $expected_columns_names)) {
+                    $columns[$value] = array_search($value, $expected_columns_names);
+                }
+            }
+        }
+
+        return $columns;
+    }
 }
