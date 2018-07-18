4dbb3d4059f12f9b96db298278b39e87ca165a22
daniildeveloper <daniildeveloper@ya.ru>
2018-07-18 16:23:05 +0600

    Accounting periods details database information

--- resources/assets/js/pages/accounting/accounting-lists.vue
+++ resources/assets/js/pages/accounting/accounting-lists.vue
@@ -1,5 +1,28 @@
 <template>
-
+  <v-data-table
+      v-bind:headers="headers"
+      :items="items"
+      hide-actions
+      class="elevation-1"
+    >
+   <template slot="items" slot-scope="props">
+      <tr @click="props.expanded = !props.expanded">
+        <td>{{ props.item.id }}</td>
+        <td class="text-xs-right">{{ props.item.created_at }}</td>
+        <td class="text-xs-right">{{ props.item.fat }}</td>
+        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-right">{{ props.item.protein }}</td>
+        <td class="text-xs-right">{{ props.item.sodium }}</td>
+        <td class="text-xs-right">{{ props.item.calcium }}</td>
+        <td class="text-xs-right">{{ props.item.iron }}</td>
+      </tr>
+    </template>
+    <template slot="expand" slot-scope="props">
+      <v-card flat>
+        <v-card-text>Peek-a-boo!</v-card-text>
+      </v-card>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -9,7 +32,21 @@ export default {
 
   data () {
     return {
-
+        headers: [
+          {
+            text: '#',
+            align: 'left',
+            sortable: false,
+            value: 'id'
+          },
+          { text: 'Date', value: 'created_at' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Sodium (mg)', value: 'sodium' },
+          { text: 'Calcium (%)', value: 'calcium' },
+          { text: 'Iron (%)', value: 'iron' }
+        ],
     }
   }
 }
