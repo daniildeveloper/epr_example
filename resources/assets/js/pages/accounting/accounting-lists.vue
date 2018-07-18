36bdb2cceeb159ce8e6de592454b9c3deb3040cf
daniildeveloper <daniildeveloper@ya.ru>
2018-07-18 16:05:08 +0600

    Created accounting period end list

---
+++ resources/assets/js/pages/accounting/accounting-lists.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+export default {
+
+  name: 'accounting-lists',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
