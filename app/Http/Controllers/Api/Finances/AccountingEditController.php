801b98302e60aae173dc71a07ee555a7495bdd47
daniildeveloper <daniildeveloper@ya.ru>
2018-07-19 15:12:44 +0600

    WIP accountings edit

--- app/Http/Controllers/Api/Finances/AccountingEditController.php
+++ app/Http/Controllers/Api/Finances/AccountingEditController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Finances;
 use App\Http\Controllers\Controller;
 use App\Models\AccountingDataHistory;
 use App\Models\AccountingPeriodEnd;
+use App\Models\AccountingPeriodEndDetail;
 use Illuminate\Http\Request;
 
 class AccountingEditController extends Controller
@@ -16,6 +17,14 @@ class AccountingEditController extends Controller
     {
         // 1. Берем все отчетные периоды и и пагинация по 5
         $accountingPeriods = AccountingPeriodEnd::with('accounting_period_end_details')->orderBy('id', 'desc')->paginate(5);
+
+        foreach ($accountingPeriods as $period) {
+            $details = AccountingPeriodEndDetail::where('accounting_id', $period->id)->get();
+
+            $period_details = [
+                ''
+            ];
+        }
 
         // 3. Возвращаем результаты
         return response()->json($accountingPeriods, 200);
